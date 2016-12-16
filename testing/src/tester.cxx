e946aed0a25e07f3b4e89e79c710d8d44a269ebd
Aaron Fienberg <atfienberg@aarons-mbp.dhcp.fnal.gov>
2016-12-16 17:09:06 -0600

    modified tester and timer

--- testing/src/tester.cxx
+++ testing/src/tester.cxx
@@ -67,7 +67,8 @@ int main() {
   auto out = tf.fit(samples, timeGuess, noise);
   std::vector<unsigned short> sampleTimes(samples.size());
   std::iota(sampleTimes.begin(), sampleTimes.end(), 0.0);
-  displayResults(tf, out, sampleTimes, samples, "test", tSpline, tSplineB);
+  displayResults(tf, out, sampleTimes, samples, "template A only", tSpline,
+                 tSplineB);
 
   cout << "testing single pulse with flat errors B: " << endl;
   time = 8 + 1 * (gRandom->Rndm() - 0.5);
@@ -87,7 +88,8 @@ int main() {
   }
   timeGuess = max_element(samples.begin(), samples.end()) - samples.begin();
   out = tf.fit(samples, timeGuess, noise);
-  displayResults(tf, out, sampleTimes, samples, "test", tSpline, tSplineB);
+  displayResults(tf, out, sampleTimes, samples, "template B only", tSpline,
+                 tSplineB);
 
   cout << "testing single pulse with flat errors half and half: " << endl;
   time = 8 + 1 * (gRandom->Rndm() - 0.5);
@@ -108,145 +110,34 @@ int main() {
   }
   timeGuess = max_element(samples.begin(), samples.end()) - samples.begin();
   out = tf.fit(samples, timeGuess, noise);
-  displayResults(tf, out, sampleTimes, samples, "test", tSpline, tSplineB);
+  displayResults(tf, out, sampleTimes, samples, "half and half", tSpline,
+                 tSplineB);
 
-  // vector<UShort_t> sampleTimes(30);
-  // iota(sampleTimes.begin(), sampleTimes.end(), 0.0);
-  // // displayResults(tf, out, sampleTimes, samples, "singlePulseFlatNoise",
-  // // tSpline);
+  cout << "testing double pulse with flat errors, A then B: " << endl;
+  time = 8 + 1 * (gRandom->Rndm() - 0.5);
+  cout << "true time 1: " << time << endl;
+  energy = 3000.0 + gRandom->Gaus(0, 0.01 * 3000);
+  cout << "true energy 1: " << energy << endl;
+  double time2 = 16 + 1 * (gRandom->Rndm() - 0.5);
+  cout << "true time 2: " << time2 << endl;
+  double energy2 = 4500.0 + gRandom->Gaus(0, 0.01 * 3000);
+  cout << "true energy 2: " << energy2 << endl;
+  pedestal = 1000.0 + 100 * (gRandom->Rndm() - 0.5);
+  cout << "true pedestal " << pedestal << endl;
+  noise = 1.65;
+  for (size_t s = 0; s < samples.size(); ++s) {
+    if ((s - time < -10) || (s - time > 90)) {
+      samples[s] = pedestal;
+    } else {
+      samples[s] = pedestal + energy * tSpline->Eval(s - time) +
+                   energy2 * tSplineB->Eval(s - time2);
+    }
+    samples[s] += gRandom->Gaus(0, noise);
+  }
 
-  // // single pulse arbitrary noise
-  // cout << "testing arbitrary sample noise, adding sqrt((s-ped)/50) worth of "
-  //         "noise" << endl;
-  // vector<double> errors(samples.size());
-  // for (size_t s = 0; s < samples.size(); ++s) {
-  //   if ((s - time < -10) || (s - time > 90)) {
-  //     samples[s] = pedestal;
-  //   } else {
-  //     samples[s] = pedestal + energy * tSpline->Eval(s - time);
-  //   }
-  //   double sampleVariance = noise * noise;
-  //   if (samples[s] - pedestal > 0) {
-  //     sampleVariance +=
-  //         (samples[s] - pedestal) * (samples[s] - pedestal) / (50 * 50);
-  //   }
-  //   double sampleUncertainty = sqrt(sampleVariance);
-  //   samples[s] += gRandom->Gaus(0, sampleUncertainty);
-  //   errors[s] = sampleUncertainty;
-  // }
-  // timeGuess = max_element(samples.begin(), samples.end()) - samples.begin();
-  // out = tf.fit(samples, timeGuess, errors);
-  // // displayResults(tf, out, sampleTimes, samples,
-  // "singlePulseArbitraryNoise",
-  // // tSpline);
-
-  // cout << "testing discontiguous fit regions (clipping) with flat noise: "
-  //      << endl;
-  // energy *= 4;
-  // cout << "new energy: " << energy << endl;
-  // for (size_t s = 0; s < samples.size(); ++s) {
-  //   if ((s - time < -10) || (s - time > 90)) {
-  //     samples[s] = pedestal;
-  //   } else {
-  //     samples[s] = pedestal + energy * tSpline->Eval(s - time);
-  //   }
-  //   samples[s] += gRandom->Gaus(0, noise);
-  //   samples[s] = samples[s] > 4096 ? 4096 : samples[s];
-  // }
-
-  // // find good samples (leave out anything over 4000)
-  // vector<UShort_t> goodSamples;
-  // vector<UShort_t> times;
-  // for (size_t s = 0; s < samples.size(); ++s) {
-  //   if (samples[s] < 4000) {
-  //     goodSamples.push_back(samples[s]);
-  //     times.push_back(s);
-  //   } else {
-  //     cout << "sample " << s << " clipping" << endl;
-  //   }
-  // }
-  // timeGuess =
-  //     max_element(goodSamples.begin(), goodSamples.end()) -
-  //     goodSamples.begin();
-  // out = tf.discontiguousFit(goodSamples, times, timeGuess, noise);
-  // // displayResults(tf, out, times, goodSamples, "clippedFitWithFlatErrors",
-  // // tSpline);
-
-  // cout << "testing clipped pulse with arbitrary errors" << endl;
-  // for (size_t s = 0; s < samples.size(); ++s) {
-  //   if ((s - time < -10) || (s - time > 90)) {
-  //     samples[s] = pedestal;
-  //   } else {
-  //     samples[s] = pedestal + energy * tSpline->Eval(s - time);
-  //   }
-  //   double sampleVariance = noise * noise;
-  //   if (samples[s] - pedestal > 0) {
-  //     sampleVariance +=
-  //         (samples[s] - pedestal) * (samples[s] - pedestal) / (50 * 50);
-  //   }
-  //   double sampleUncertainty = sqrt(sampleVariance);
-  //   samples[s] += gRandom->Gaus(0, sampleUncertainty);
-  //   errors[s] = sampleUncertainty;
-  //   samples[s] = samples[s] > 4096 ? 4096 : samples[s];
-  // }
-  // vector<double> goodErrors;
-  // goodSamples.resize(0);
-  // times.resize(0);
-  // for (size_t s = 0; s < samples.size(); ++s) {
-  //   if (samples[s] < 4000) {
-  //     goodSamples.push_back(samples[s]);
-  //     times.push_back(s);
-  //     goodErrors.push_back(errors[s]);
-  //   } else {
-  //     cout << "sample " << s << " clipping, ignoring for fit" << endl;
-  //   }
-  // }
-  // timeGuess =
-  //     max_element(goodSamples.begin(), goodSamples.end()) -
-  //     goodSamples.begin();
-  // out = tf.discontiguousFit(goodSamples, times, timeGuess, goodErrors);
-  // // displayResults(tf, out, times, goodSamples,
-  // "clippedWithArbitraryErrors",
-  // // tSpline);
-
-  // cout << "testing double pulse fit with flat errors: " << endl;
-  // energy = energy / 4;
-  // cout << "True E1 : " << energy << endl;
-  // double energy2 = energy * (0.5 + 0.4 * (gRandom->Rndm() - 0.5));
-  // cout << "True E2 : " << energy2 << endl;
-  // cout << "True time 1: " << time << endl;
-  // double time2 = time + 2.5 + 0.5 * (gRandom->Rndm() - 0.5);
-  // cout << "true time 2: " << time2 << endl;
-  // cout << "true pedestal : " << pedestal << endl;
-  // for (std::size_t s = 0; s < samples.size(); ++s) {
-  //   if ((s - time2 < -10) || (s - time > 90)) {
-  //     samples[s] = pedestal;
-  //   } else {
-  //     samples[s] = energy * tSpline->Eval(s - time) +
-  //                  energy2 * tSpline->Eval(s - time2) + pedestal;
-  //   }
-  //   samples[s] += gRandom->Gaus(0, noise);
-  // }
-  // double max = max_element(samples.begin(), samples.end()) - samples.begin();
-  // vector<double> tGuesses = {max - 2, max + 2};
-  // out = tf.fit(samples, tGuesses, noise);
-  // // displayResults(tf, out, sampleTimes, samples, "doublePulseFlatErrors",
-  // // tSpline);
-
-  // cout << "testing three pulse fit, adding a third pulse. " << endl;
-  // double energy3 = 0.25 * (energy + 0.4 * (gRandom->Rndm() - 0.5));
-  // cout << "energy 3: " << energy3 << endl;
-  // double time3 = time2 + 10 + 4 * (gRandom->Rndm() - 0.5);
-  // cout << "time 3: " << time3 << endl;
-  // for (std::size_t s = 0; s < samples.size(); ++s) {
-  //   if (!((s - time3 < -10) || (s - time3 > 90))) {
-  //     samples[s] += energy3 * tSpline->Eval(s - time3);
-  //   }
-  // }
-  // tGuesses = {8, 10.5, 20.5};
-  // out = tf.fit(samples, tGuesses, noise);
-  // // displayResults(tf, out, sampleTimes, samples, "triplePulseFlatErrors",
-  // // tSpline);
+  out = tf.fit(samples, std::vector<double>{8, 16}, noise);
+  displayResults(tf, out, sampleTimes, samples, "first A, second B", tSpline,
+                 tSplineB);
 
   outf->Write();
 }
@@ -254,28 +145,39 @@ int main() {
 void displayResults(TemplateFitter& tf, TemplateFitter::Output out,
                     std::vector<UShort_t> sampleTimes,
                     std::vector<UShort_t> trace, string title,
-                    TSpline3* tSpline, TSpline3* tSplineB) {
+                    TSpline3* tSplineA, TSpline3* tSplineB) {
   const int nPulses = out.times.size();
+  int min = *std::min_element(sampleTimes.begin(), sampleTimes.end());
+  int max = *std::max_element(sampleTimes.begin(), sampleTimes.end());
   // print to terminal
   cout << endl;
+  std::vector<double> sigA;
+  std::vector<double> sigB;
+  std::vector<double> sigT;
+  std::vector<double> sigTot;
+
   for (int i = 0; i < nPulses; ++i) {
-    cout << "t" << i + 1 << ": " << out.times[0] << " +/- "
-         << sqrt(tf.getCovariance(i, i)) << endl;
+    sigT.push_back(sqrt(tf.getCovariance(i, i)));
+    cout << "t" << i + 1 << ": " << out.times[i] << " +/- " << sigT.back()
+         << endl;
 
     double varEA = tf.getCovariance(2 * i + nPulses, 2 * i + nPulses);
+    sigA.push_back(sqrt(varEA));
     cout << "energyA" << i + 1 << ": " << out.scalesA[i] << " +/- "
-         << sqrt(varEA) << endl;
+         << sigA.back() << endl;
 
     double varEB = tf.getCovariance(2 * i + 1 + nPulses, 2 * i + 1 + nPulses);
+    sigB.push_back(sqrt(varEB));
     cout << "energyB" << i + 1 << ": " << out.scalesB[i] << " +/- "
-         << sqrt(varEB) << endl;
+         << sigB.back() << endl;
 
     double correlation = tf.getCovariance(2 * i + nPulses, 2 * i + 1 + nPulses);
+    sigTot.push_back(sqrt(varEA + varEB + 2 * correlation));
     cout << "total energy: " << out.scalesA[i] + out.scalesB[i] << " +/- "
-         << sqrt(varEA + varEB + correlation) << std::endl;
+         << sigTot.back() << std::endl;
   }
-  cout << "pedestal: " << out.pedestal << " +/- "
-       << sqrt(tf.getCovariance(3 * nPulses, 3 * nPulses)) << endl;
+  double sigPed = sqrt(tf.getCovariance(3 * nPulses, 3 * nPulses));
+  cout << "pedestal: " << out.pedestal << " +/- " << sigPed << endl;
   cout << "chi2: " << out.chi2 << std::endl;
   cout << endl;
   cout << "covariance matrix" << endl;
@@ -287,77 +189,76 @@ void displayResults(TemplateFitter& tf, TemplateFitter::Output out,
   }
   cout << endl;
 
-  // // make plot
-  // unique_ptr<TCanvas> c(
-  //     new TCanvas((title + "_canvas").c_str(), (title + "_canvas").c_str()));
+  // make plot
+  unique_ptr<TCanvas> c(
+      new TCanvas((title + "_canvas").c_str(), (title + "_canvas").c_str()));
 
-  // unique_ptr<TGraph> g(new TGraph(0));
-  // g->SetTitle(title.c_str());
-  // for (size_t i = 0; i < trace.size(); ++i) {
-  //   g->SetPoint(g->GetN(), sampleTimes[i], trace[i]);
-  // }
+  unique_ptr<TGraph> g(new TGraph(0));
+  g->SetTitle(title.c_str());
+  for (size_t i = 0; i < trace.size(); ++i) {
+    g->SetPoint(g->GetN(), sampleTimes[i], trace[i]);
+  }
 
-  // // room for up to three pulses
-  // auto templateFunction = [&](double* x, double* p) {
-  //   return p[6] + p[1] * tSpline->Eval(x[0] - p[0]) +
-  //          p[3] * tSpline->Eval(x[0] - p[2]) +
-  //          (x[0] - p[4] > -10 ? p[5] * tSpline->Eval(x[0] - p[4]) : 0);
-  // };
-  // unique_ptr<TF1> func(new TF1("fitFunc", templateFunction, 0, 30, 7));
-  // func->SetLineColor(kBlack);
-  // func->SetParameters(vector<double>(7, 0).data());
+  // template function
+  auto templateFunction = [&](double* x, double* p) {
+    double pedestal = p[0];
+    double pulseVal = pedestal;
+    double t = x[0];
+    for (int pulse = 0; pulse < nPulses; ++pulse) {
+      double* pulseParams = p + 1 + 3 * pulse;
+      double tp = t - pulseParams[0];
+      if (tp > -10 && tp < 90) {
+        pulseVal += pulseParams[1] * tSplineA->Eval(tp) +
+                    pulseParams[2] * tSplineB->Eval(tp);
+      }
+    }
+    return pulseVal;
+  };
+  unique_ptr<TF1> func(
+      new TF1("fitFunc", templateFunction, min, max, 3 * nPulses + 1));
+  func->SetLineColor(kMagenta + 2);
+  func->SetParameter(0, out.pedestal);
+  for (int i = 0; i < nPulses; ++i) {
+    func->SetParameter(1 + 3 * i, out.times[i]);
+    func->SetParameter(1 + 3 * i + 1, out.scalesA[i]);
+    func->SetParameter(1 + 3 * i + 2, out.scalesB[i]);
+  }
 
-  // g->SetMarkerStyle(20);
-  // g->Draw("ap");
-  // g->GetXaxis()->SetTitle("sample number");
-  // g->GetYaxis()->SetTitle("ADC counts");
-  // g->GetYaxis()->SetTitleOffset(1.5);
+  g->SetMarkerStyle(20);
+  g->Draw("ap");
+  g->GetXaxis()->SetTitle("sample number");
+  g->GetYaxis()->SetTitle("ADC counts");
+  g->GetYaxis()->SetTitleOffset(1.5);
+  g->GetXaxis()->SetRangeUser(min, max);
+  func->SetNpx(10000);
+  func->Draw("same");
 
-  // double yMin = g->GetYaxis()->GetXmin();
-  // double yMax = g->GetYaxis()->GetXmax();
-  // std::unique_ptr<TPaveText> txtbox(new TPaveText(
-  //     15, yMin + (yMax - yMin) * 0.9, 29, yMin + (yMax - yMin) * 0.5));
-  // txtbox->SetFillColor(kWhite);
-  // func->SetParameter(6, out.pedestal);
-  // for (int i = 0; i < nPulses; ++i) {
-  //   func->SetParameter(2 * i, out.times[i]);
-  //   txtbox->AddText(Form("t_{%i}: %.3f #pm %.3f", i + 1, out.times[i],
-  //                        sqrt(tf.getCovariance(i, i))));
-  //   func->SetParameter(2 * i + 1, out.scales[i]);
-  //   txtbox->AddText(Form("E_{%i}: %.0f #pm %.0f", i + 1, out.scales[i],
-  //                        sqrt(tf.getCovariance(nPulses + i, nPulses + i))));
-  // }
-  // txtbox->AddText(Form("pedestal: %.0f #pm %.1f", out.pedestal,
-  //                      sqrt(tf.getCovariance(2 * nPulses, 2 * nPulses))));
-  // txtbox->AddText(Form("#chi^{2} / NDF : %.2f", out.chi2));
+  double yMin = g->GetYaxis()->GetXmin();
+  double yMax = g->GetYaxis()->GetXmax();
+  std::unique_ptr<TPaveText> txtbox(new TPaveText(
+      22, yMin + (yMax - yMin) * 0.9, 43, yMin + (yMax - yMin) * 0.5));
+  txtbox->SetFillColor(kWhite);
+  for (int i = 0; i < nPulses; ++i) {
+    txtbox->AddText(
+        Form("EA_{%i}: %.0f #pm %.0f", i + 1, out.scalesA[i], sigA[i]));
+    txtbox->AddText(
+        Form("EB_{%i}: %.0f #pm %.0f", i + 1, out.scalesB[i], sigB[i]));
+    txtbox->AddText(Form("ETOTAL_{%i}: %.0f #pm %.0f", i + 1,
+                         out.scalesB[i] + out.scalesA[i], sigTot[i]));
+  }
+  txtbox->AddText(Form("pedestal: %.0f #pm %.1f", out.pedestal, sigPed));
+  txtbox->AddText(Form("#chi^{2} / NDF : %.2f", out.chi2));
 
-  // std::vector<unique_ptr<TF1>> components;
-  // if (nPulses > 1) {
-  //   int colors[3] = {kRed, kBlue, kMagenta + 2};
-  //   for (int i = 0; i < nPulses; ++i) {
-  //     components.emplace_back(new TF1("fitFunc", templateFunction, 0, 30,
-  //     7));
-  //     components.back()->SetParameters(vector<double>(7, 0).data());
-  //     components.back()->SetParameter(6, out.pedestal);
-  //     components.back()->SetParameter(2 * i, out.times[i]);
-  //     components.back()->SetParameter(2 * i + 1, out.scales[i]);
-  //     components.back()->SetLineColor(colors[i]);
-  //     components.back()->SetNpx(1000);
-  //     components.back()->Draw("same");
-  //   }
-  // }
+  txtbox->Draw("same");
+  c->Print((title + ".pdf").c_str());
+  c->Print((title + ".png").c_str());
+  c->Write();
 
-  // func->SetNpx(1000);
-  // func->Draw("same");
-  // txtbox->Draw("same");
-  // c->Print((title + ".pdf").c_str());
-  // c->Write();
+  c->Modified();
+  c->Update();
+  c->Draw();
+  gSystem->ProcessEvents();
 
-  // c->Modified();
-  // c->Update();
-  // c->Draw();
-  // gSystem->ProcessEvents();
-
-  // cout << title << " displayed. Any key to move on..." << endl;
+  cout << title << " displayed. Any key to move on..." << endl;
   cin.ignore();
 }
--- testing/src/timer.cxx
+++ testing/src/timer.cxx
@@ -9,7 +9,7 @@
 #include <chrono>
 #include <iostream>
 
-typedef struct{
+typedef struct {
   double time;
   double energy;
   double chi2;
@@ -18,80 +18,84 @@ typedef struct{
 using namespace std;
 
 namespace {
-  vector<vector<TemplateFitter::Output>> outs;
+vector<vector<TemplateFitter::Output>> outs;
 }
 
-void fit(TemplateFitter tf, vector<UShort_t> samples, 
-  double timeGuess, double noise, vector<TemplateFitter::Output>& out_vect){
+void fit(TemplateFitter tf, vector<UShort_t> samples, double timeGuess,
+         double noise, vector<TemplateFitter::Output>& out_vect) {
   out_vect.resize(100000);
-  for(int i = 0; i < 100000; ++i){
+  for (int i = 0; i < 100000; ++i) {
     out_vect[i] = tf.fit(samples, timeGuess, noise);
   }
 }
 
-int main(){
-
-  TFile* splineF = new TFile("../adjustedBoardTemplate10_26_15.root");
-  TSpline3* tSpline = (TSpline3*) splineF->Get("masterSpline");
-
-  TemplateFitter tf(tSpline, -10, 90);
+int main() {
+  TFile splineF("../beamTemplateFile37.root");
+  TSpline3* tSpline = (TSpline3*)splineF.Get("masterSpline");
+  TFile splineF2("../beamleakTemplateFile37.root");
+  TSpline3* tSplineB = (TSpline3*)splineF2.Get("masterSpline");
+  TemplateFitter tf(tSpline, tSplineB, -10, 90);
   tf.setAccuracy(1e-2);
-  
+
   gRandom->SetSeed(19);
   //  tf.setAccuracy(1e-6);
-  double time = 50 + 10*(gRandom->Rndm() - 0.5);
+  double time = 50 + 10 * (gRandom->Rndm() - 0.5);
   cout << "time: " << time << endl;
-  double energy = 3000.0 + gRandom->Gaus(0, 0.01*3000);
+  double energy = 3000.0 + gRandom->Gaus(0, 0.01 * 3000);
   cout << "energy : " << energy << endl;
-  double pedestal = 1000.0 + 100*(gRandom->Rndm() - 0.5);
+  double pedestal = 1000.0 + 100 * (gRandom->Rndm() - 0.5);
   cout << "pedestal " << pedestal << endl;
 
   double noise = 1.65;
   vector<UShort_t> samples(100);
-  for(size_t s = 0; s < 100; ++s) { 
-    if((s - time < -10) || (s - time > 90) ){
+  for (size_t s = 0; s < 100; ++s) {
+    if ((s - time < -10) || (s - time > 90)) {
       samples[s] = 0;
+    } else {
+      samples[s] = energy * tSpline->Eval(s - time);
     }
-    else{
-      samples[s] = energy * tSpline->Eval(s - time); 
-    }
-    samples[s] +=  gRandom->Gaus(0, noise) + pedestal;
+    samples[s] += gRandom->Gaus(0, noise) + pedestal;
   }
   auto maxiter = max_element(samples.begin(), samples.end());
   int fitStart = maxiter - 5 - samples.begin();
-  rotate(samples.begin(), maxiter-5, samples.end());
+  rotate(samples.begin(), maxiter - 5, samples.end());
   samples.resize(42);
-  
+
   double timeGuess = 5;
-  
+
   // TemplateFitter::Output out;
   auto t1 = chrono::high_resolution_clock::now();
 
   vector<thread> threads;
 
   outs.resize(8);
-  for(int i = 0; i < 8; ++i){
+  for (int i = 0; i < 8; ++i) {
     threads.push_back(thread(fit, tf, samples, timeGuess, noise, ref(outs[i])));
   }
 
-  for(auto& t: threads){
+  for (auto& t : threads) {
     t.join();
   }
   auto t2 = chrono::high_resolution_clock::now();
-  cout << 800000/chrono::duration<double>(t2-t1).count() << " fits per sec " << endl;
-  cout << 800000/chrono::duration<double>(t2-t1).count()/4.0 << " fits per sec over 4" << endl;
+  cout << 800000 / chrono::duration<double>(t2 - t1).count() << " fits per sec "
+       << endl;
+  cout << 800000 / chrono::duration<double>(t2 - t1).count() / 4.0
+       << " fits per sec over 4" << endl;
 
   auto out = outs.back().back();
 
   cout << "t: " << out.times[0] + fitStart << endl;
-  cout << "scale: " << out.scales[0] << endl;
+  cout << "scale: " << out.scalesA[0] << endl;
   cout << "chi2: " << out.chi2 << endl;
 
-  // cout << " t = " << out.times[0] + fitStart << " +/- " << sqrt(tf.getCovariance(0,0)) << endl;
-  // cout << " s = " << out.scales[0] << " +/- " << sqrt(tf.getCovariance(1,1)) << endl;
-  // cout << " p = " << out.pedestal << " +/- " << sqrt(tf.getCovariance(2,2)) << endl;
+  // cout << " t = " << out.times[0] + fitStart << " +/- " <<
+  // sqrt(tf.getCovariance(0,0)) << endl;
+  // cout << " s = " << out.scales[0] << " +/- " << sqrt(tf.getCovariance(1,1))
+  // << endl;
+  // cout << " p = " << out.pedestal << " +/- " << sqrt(tf.getCovariance(2,2))
+  // << endl;
   // cout << " chi2: " << out.chi2 << endl;
-  
+
   // cout << "cov" <<endl;
   // for(int i = 0; i < 3; ++i){
   //   for(int j = 0; j < 3; ++j){
@@ -99,6 +103,4 @@ int main(){
   //   }
   //   cout << endl;
   // }
-    
-
 }
